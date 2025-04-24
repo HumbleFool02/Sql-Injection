9f1137daa1b4966b18e041489a1f1e8742f88435
Piyush Kasle <87870969+HumbleFool02@users.noreply.github.com>
2025-04-24 19:39:23 -0400

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,4 @@
 DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS secrets;
 
 CREATE TABLE users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -8,16 +7,6 @@ CREATE TABLE users (
     role TEXT NOT NULL
 );
 
-CREATE TABLE secrets (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    value TEXT NOT NULL
-);
-
--- Seed users
 INSERT INTO users (username, password, role) VALUES ('admin', 'admin', 'admin');
 INSERT INTO users (username, password, role) VALUES ('user', 'user', 'user');
 INSERT INTO users (username, password, role) VALUES ('username', 'password', 'user');
-
--- Store the real flag
-INSERT INTO secrets (name, value) VALUES ('ctf_flag', 'flag{you_fetched_it}');
