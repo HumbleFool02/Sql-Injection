04f2bae8e3b11ac065c363a3505ffeb819aeac77
Piyush Kasle <87870969+HumbleFool02@users.noreply.github.com>
2025-04-24 19:38:38 -0400

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -7,11 +7,8 @@ with open("schema.sql") as f:
 
 cur = connection.cursor()
 
-# Insert admin and regular user
 # cur.execute("INSERT INTO users (username, password, role) VALUES (?, ?, ?)", ("admin", "admin", "admin"))
 # cur.execute("INSERT INTO users (username, password, role) VALUES (?, ?, ?)", ("user", "user", "user"))
 
 connection.commit()
 connection.close()
-
-print("Database initialized with users and roles.")
